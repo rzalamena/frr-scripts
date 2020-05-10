b9f82210492dbca0ee422764982cb9e4a413323d
Rafael Zalamena <rzalamena@gmail.com>
2020-05-10 17:36:19 -0300

    compile.sh: use new shell scripting style
    Switch from Slackware styled scripts to something more user-friendly:
    don't use environment variables to control variables and use command
    line options for that purpose.

--- compile.sh
+++ compile.sh
@@ -14,50 +14,88 @@
 # OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
 
+usage() {
+  cat <<EOF
+Usage: $0
+  [-h] [--asan] [--doc] [--fpm] [--help] [--jobs=NUMBER] [--snmp] [--systemd]
+
+Options:
+  --asan: build FRR with address sanitizer.
+  --doc: configure FRR to enable documentation builds (requires sphinx).
+  --fpm: build FRR with forwarding plane manager.
+  --help or -h: this help message.
+  --jobs: amount of parallel build jobs (defaults to $jobs).
+  --snmp: build FRR with SNMP support.
+  --systemd: build FRR with systemd support.
+EOF
+  exit 1
+}
+
 # Quit on errors.
 set -e
 
 # Set variables.
-JOBS=${JOBS:-2}
-ASAN=${ASAN:-no}
-DOC=${DOC:-no}
-FPM=${FPM:-no}
-SNMP=${SNMP:-no}
-SYSTEMD=${SYSTEMD:-no}
-SCAN_BUILD=${SCAN_BUILD:-no}
+flags=()
+jobs=2
+scan_build=no
+
+longopts='asan,doc,fpm,help,jobs:,scan-build,snmp,systemd'
+shortopts='h'
+options=$(getopt -u --longoptions "$longopts" "$shortopts" $*)
+if [ $? -ne 0 ]; then
+  usage
+  exit 1
+fi
+
+set -- $options
+while [ $# -ne 0 ]; do
+  case "$1" in
+    --asan)
+      flags+=(--enable-address-sanitizer);
+      shift
+      ;;
+    --doc)
+      flags+=(--enable-doc)
+      shift
+      ;;
+    --fpm)
+      flags+=(--enable-fpm)
+      shift
+      ;;
+    --jobs)
+      jobs="$2"
+      shift 2
+      ;;
+    --scan-build)
+      scan_build=yes
+      shift
+      ;;
+    --snmp)
+      flags+=(--enable-snmp=agentx)
+      shift
+      ;;
+    --systemd)
+      flags+=(--enable-systemd)
+      shift
+      ;;
+    -h | --help)
+      usage
+      shift
+      ;;
+
+    --) shift ;;
+    *) echo "unhandled argument '$1'" 2>&1 ; exit 1 ;;
+  esac
+done
 
 # Bootstrap the configure file.
 if [ ! -f configure ]; then
   ./bootstrap.sh
 fi
 
-# Configure FRR build.
-FLAGS=()
-if [ $ASAN = 'yes' ]; then
-  FLAGS+=(--enable-address-sanitizer)
-fi
-
-if [ $DOC = 'yes' ]; then
-  FLAGS+=(--enable-doc)
-fi
-
-if [ $FPM = 'yes' ]; then
-  FLAGS+=(--enable-fpm)
-fi
-
-if [ $SNMP = 'yes' ]; then
-  FLAGS+=(--enable-snmp=agentx)
-fi
-
-if [ $SYSTEMD = 'yes' ]; then
-  FLAGS+=(--enable-systemd)
-else
-  FLAGS+=(--enable-systemd=no)
-fi
-
 if [ ! -f Makefile ]; then
   ./configure \
-    ${FLAGS[@]} \
+    ${flags[@]} \
     --enable-multipath=64 \
     --prefix=/usr \
     --localstatedir=/var/run/frr \
@@ -74,8 +112,10 @@ if [ ! -f Makefile ]; then
     --with-pkg-git-version
 fi
 
-if [ $SCAN_BUILD = 'no' ]; then
-  make -j$JOBS
+if [ $scan_build = 'no' ]; then
+  make --jobs=$jobs --load-average=$jobs
 else
-  scan-build -maxloop 128 make -j$JOBS
+  scan-build -maxloop 128 make --jobs=$jobs
 fi
+
+exit 0
